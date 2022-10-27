0f7a92eeb179de3a835613ad0d1bf700cc7a5826
Fahd <36855117+fqhd@users.noreply.github.com>
2022-10-28 00:17:00 +0100

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,9 +3,9 @@
 #include "stb_image_write.h"
 #include <stdint.h>
 
-const uint32_t boxSize = 1000;
-const uint32_t maxIterations = 300;
-const double infinityValue = 50.0;
+const uint32_t boxSize = 2000;
+const uint32_t maxIterations = 100;
+const double infinityValue = 20.0;
 
 void drawMandelBrot(uint8_t* data, double mapping);
 double map(double value, double iStart, double iStop, double oStart, double oStop);
@@ -15,7 +15,7 @@ void HSVtoRGB(float h, float s, float v, uint8_t& r, uint8_t& g, uint8_t& b);
 int main() {
     uint8_t* data = new uint8_t[boxSize * boxSize * 3];
 
-    drawMandelBrot(data, 2.0);
+    drawMandelBrot(data, 0.001);
 
     stbi_write_jpg("out.jpg", boxSize, boxSize, 3, data, 100);
     return 0;
@@ -24,8 +24,8 @@ int main() {
 void drawMandelBrot(uint8_t* data, double mapping) {
     for(uint32_t x = 0; x < boxSize; x++){
         for(uint32_t y = 0; y < boxSize; y++){
-            double a = map((double)x, 0.0, boxSize, -mapping, mapping);
-            double b = map((double)y, 0.0, boxSize, -mapping, mapping);
+            double a = map((double)x, 0.0, boxSize, -mapping, mapping) - 0.7453;
+            double b = map((double)y, 0.0, boxSize, -mapping, mapping) - 0.1127;
 
             double cA = a;
             double cB = b;
@@ -42,11 +42,11 @@ void drawMandelBrot(uint8_t* data, double mapping) {
                 }
             }
 
-            float hue = int(255.0f * n / (float)maxIterations);
-            float saturation = 255.0f;
+            float hue = n / (float)maxIterations;
+            float saturation = 1.0f;
             float value;
             if(n < maxIterations){
-                value = 255.0f;
+                value = 1.0f;
             }else{
                 value = 0.0f;
             }
@@ -54,7 +54,7 @@ void drawMandelBrot(uint8_t* data, double mapping) {
             uint8_t red, green, blue;
             HSVtoRGB(hue, saturation, value, red, green, blue);
 
-            setPixel(data, x, y, red, green, blue);
+            setPixel(data, x, y, green, red, blue);
         }
     }
 }
@@ -80,28 +80,28 @@ void HSVtoRGB(float h, float s, float v, uint8_t& r, uint8_t& g, uint8_t& b) {
     int index = ((int)i) % 6;
 
     if(index == 0){
-        r = v;
-        g = t;
-        b = p;
+        r = v * 255.0f;
+        g = t * 255.0f;
+        b = p * 255.0f;
     }else if(index == 1){
-        r = q;
-        g = v;
-        b = p;
+        r = q * 255.0f;
+        g = v * 255.0f;
+        b = p * 255.0f;
     }else if(index == 2){
-        r = p;
-        g = v;
-        b = t;
+        r = p * 255.0f;
+        g = v * 255.0f;
+        b = t * 255.0f;
     }else if(index == 3){
-        r = p;
-        g = q;
-        b = v;
+        r = p * 255.0f;
+        g = q * 255.0f;
+        b = v * 255.0f;
     }else if(index == 4){
-        r = t;
-        g = p;
-        b = v;
+        r = t * 255.0f;
+        g = p * 255.0f;
+        b = v * 255.0f;
     }else if(index == 5){
-        r = v;
-        g = p;
-        b = q;
+        r = v * 255.0f;
+        g = p * 255.0f;
+        b = q * 255.0f;
     }
 }
